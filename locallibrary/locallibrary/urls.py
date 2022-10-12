487f444dba2e0a24179e2d2a71a48e028128540d
alikpid <koromislo.molodec@gmail.com>
2022-10-12 13:52:51 +0700

    причёсывание

--- locallibrary/locallibrary/urls.py
+++ locallibrary/locallibrary/urls.py
@@ -26,6 +26,10 @@ urlpatterns = [
     path('', RedirectView.as_view(url='/catalog/', permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
+
 
 
 
