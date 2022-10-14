7e2f406db98f702da77b3ea5aa88fe5a34d00224
AlinaK <koromislo.molodec@gmail.com>
2022-10-15 02:00:23 +0700

    кнопочка редактирования книги и автора

--- catalog/urls.py
+++ catalog/urls.py
@@ -18,7 +18,7 @@ urlpatterns += [
 ]
 urlpatterns += [
     path('author/create/', views.AuthorCreate.as_view(), name='author-create'),
-    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author_update'),
+    url(r'^author/(?P<pk>\d+)/update/$', views.AuthorUpdate.as_view(), name='author-update'),
     url(r'^author/(?P<pk>\d+)/delete/$', views.AuthorDelete.as_view(), name='author_delete'),
 ]
 urlpatterns += [
